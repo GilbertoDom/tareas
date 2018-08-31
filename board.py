9b2c77f528646b7e8cc24589e5d39e5a327c44bd
Gilberto Dominguez <gilberto.carlos@comunidad.unam.mx>
2018-08-30 23:28:45 -0500

    avance
    Error en la funcion gen_state, al llamar la funcion make_board modifica el estado original y toma al primer hijo

--- board.py
+++ board.py
@@ -8,25 +8,71 @@ import copy
 
 
 class Board:
+    global x,y 
+    #global y 
+    x, y = 1, 1
     def __init__(self, state):
         self.state = copy.deepcopy(state)
-        self.x = 1
-        self.y = 1
         self.state_current_0 = self.get_index_of_0()
 
     def state_expand(self):
+        list_of_children = []
         s = self.state
-        self.evaluate_moves(self.get_index_of_0())
+        possible_states = self.evaluate_moves(self.get_index_of_0())
+        print(possible_states)
+        
+    def gen_state(self, moves, current_index, current_state):
+        def make_board(move, index, state):
+            s = state
+            curr_x, curr_y = index[0], index[1]
+            new_x, new_y = move[0], move[1]
+            aux = state[new_x][new_y]
+            #print (aux)
+            s[curr_x][curr_y] = aux
+            s[new_x][new_y] = 0
+            #print (state)
+            #print(s)
+            return s
+        states = []
+        for move in moves:
+            print(current_state)
+            child_state = make_board(move, current_index, current_state)
+            print(child_state)
+            states.append(child_state)
+        return states
+        
+        
     def evaluate_moves(self, index):
         if isinstance(index, str):
-            return None
+            return index
         else:
+            current_state = self.state
             current_x, current_y = index[0], index[1]
-            move_up = (current_x - self.x, current_y)
-            move_down = (current_x + self.x, current_y)
-            move_right = (current_x, current_y + self.y)
-            move_left = (current_x, current_y - self.y)
-            print(move_up, move_down, move_right, move_left)      
+            move_up = (current_x - x, current_y)
+            move_down = (current_x + x, current_y)
+            move_right = (current_x, current_y + y)
+            move_left = (current_x, current_y - y)
+            moves = [move_up, move_down, move_right, move_left]    
+            valid_moves = self.is_valid(moves)
+            print()
+            print(current_state)
+            print()
+            states = self.gen_state(valid_moves, index, current_state)
+            print()
+            print(current_state)
+            return states
+        
+    def is_valid(self, list_of_moves):
+        valid_moves = []
+        for move in list_of_moves:
+            x, y = move[0], move[1]
+            if x not in [0,1,2,3]:
+                continue
+            elif y not in [0,1,2,3]:
+                continue
+            else:
+                valid_moves.append(move)
+        return valid_moves
         
     def get_index_of_0(self):
         i = 0
