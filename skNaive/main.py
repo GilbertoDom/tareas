c230169ad6f89988e4ec1a9a8bcc5b3d34c545a0
Gilberto Carlos Domínguez Aguilar <gilberto.carlos@comunidad.unam.mx>
2018-10-08 01:26:22 -0500

    terminado
    Clasificaciones con curvas ROC

--- skNaive/main.py
+++ skNaive/main.py
@@ -13,7 +13,12 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 import preprocessing
+import matplotlib.pyplot as plt
+import numpy as np
 
+
+#global x, y
+#x, y = [], []
 
 def parse_vetorizer(vname, vectorizer, body, clase):
 	
@@ -40,18 +45,41 @@ def parse_vetorizer(vname, vectorizer, body, clase):
 			else:
 				fp += 1
 	recall = tp / (tp+fn)
+	#y.append(recall)
 	false_alarm = fp / (fp+tn)
+	#x.append(false_alarm)
 	precision = tp / (tp+fp)
 	f1score = (2 * (recall*precision))/(recall+precision)
 	accuaracy = (tp + tn) / (tp + fp + tn + fn)
 	s = "Resultados del clasificador {}:\n\tExactitud: {:0.2f}%\n\tSensibilidad : {:0.2f}%\n\tFalsa Alarma: {:0.2f}%\n\tPrecisión: {:0.2f}%\n\tF1 Score: {:0.2f}\n".format(vname, accuaracy*100, recall*100, false_alarm*100, precision*100, f1score)
 	print (s)
 
+	plt.scatter(false_alarm, recall, label=vname, s=25, marker='o')
+
+
+def plot_ROC():
+	#plt.scatter(x, y, label='Clasificadores obtenidos', color='k', s=25, marker='o')
+	plt.scatter([0],[1], label='CLasificador perfecto', color='g', s=30, marker='o')
+	#plt.plot()
+	xl = np.linspace(0, 1, 100)
+	plt.plot(xl, xl, label='adivinar', color='r', )
+
+	axes = plt.gca()
+	axes.set_xlim([-0.01,1.1])
+	axes.set_ylim([-0.01,1.1])
+
+	plt.xlabel('FP Rate')
+	plt.ylabel('TP Rate')
+	plt.title('Curvas ROC')
+	plt.legend()
+	plt.show()
+
 		
 if __name__ == '__main__':
 
 	clase = preprocessing.clase
 	mensajes = preprocessing.body
+	print(len(mensajes))
 
 	vect_with_sw = CountVectorizer(lowercase=True, decode_error='ignore')
 	vect_wo_sw = CountVectorizer(stop_words='english', lowercase=True, decode_error='ignore')
@@ -65,5 +93,5 @@ if __name__ == '__main__':
 	for vname, vectorizer in vecs.items():
 		parse_vetorizer(vname, vectorizer, mensajes, clase)
 
-
+	plot_ROC()
 
